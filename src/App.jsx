f24565a3c7ff3f93fc35fed0baa7c81d350d3371
tanopanta <25007602+tanopanta@users.noreply.github.com>
2021-09-21 15:48:43 +0900

    apply styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 const App = () => {
+  const onClickButton = () => alert();
+  const contectStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+
   return (
     <>
-      <h1>こんにちは！!</h1>
-      <p>お元気ですか？</p>
+      <h1 style={{ color: "red" }}>こんにちは！!</h1>
+      <p style={contectStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
